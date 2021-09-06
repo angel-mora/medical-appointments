c6a2c8ac20e871ed8e77e66a9036c8c68eac9486
AngelMoraDev <angelmomaa@gmail.com>
2021-09-06 09:35:38 -0500

    add association in the models

--- app/models/physician.rb
+++ app/models/physician.rb
@@ -1,2 +1,8 @@
 class Physician < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :appointments
+  has_many :users, through: :appointments
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :physicians
+  root 'appointments#index'
   resources :physicians
   resources :appointments
   devise_for :users
